77def92772f9e8a5fddf74f27e84a3316d079cf3
ivwu22 <ivywu6@gmail.com>
2021-07-19 11:43:28 -0400

    sets up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
   return (
     <>
       <header>
         <h1>Dronology</h1>
-        <nav></nav>
       </header>
+      <Navigation />
+      <main>
+        <Switch></Switch>
+      </main>
     </>
   );
 }
