ae45d1cca17dacd4ca2a1d1712f2753bf17d748b
Andre Brown <andreups@msn.com>
2021-07-21 14:40:01 -0400

    added functionality to delete button

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -12,17 +12,31 @@ class CustomersList extends Component {
       .catch(console.error);
   }
 
+  deleteCustomer(id, e) {
+    CustomerDataService.delete(id).then((res) => {
+      console.log(res);
+      console.log(res.data);
+      const customers = this.state.customers.filter(
+        (customer) => customer.id !== id
+      );
+      this.setState({ customers });
+    });
+  }
+
   render() {
     const { customers } = this.state;
     const customerListItems = customers.map((customer, index) => (
       <li key={`${customer.phone}-${index}`}>
+        <p>Customer ID: {customer.id}</p>
         <p>First Name: {customer.first_name}</p>
         <p>Middle Name: {customer.middle_name}</p>
         <p>Last Name: {customer.last_name}</p>
         <p>Phone: {customer.phone}</p>
         <p>Email: {customer.email}</p>
         <p>Notes: {customer.notes}</p>
-        <button>Delete</button>
+        <button onClick={(e) => this.deleteCustomer(customer.id, e)}>
+          Delete
+        </button>
       </li>
     ));
     return (
