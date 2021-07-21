2d7439062a6ef28f3c9ee88225ab8552b861ca45
Joyce Young <jjyoung145@gmail.com>
2021-07-21 14:22:43 -0400

    started connection into customerList

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import customerDataService from "../../services/customer.data.service";
 
 class CustomersList extends Component {
+  state = {
+    customers: [],
+  };
+
   render() {
-    return <div>CustomersList</div>;
+    return <div></div>;
   }
 }
 
