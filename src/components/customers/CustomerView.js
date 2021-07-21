937f3963cf1b1e9df23a087de52345df16db0e84
dl86644n <dana.lopez@aol.com>
2021-07-21 11:27:19 -0400

    implement state

--- src/components/customers/CustomerView.js
+++ src/components/customers/CustomerView.js
@@ -4,18 +4,20 @@ import { Card, Button, Container } from "react-bootstrap";
 
 class CustomerView extends Component {
   state = {
-    // firstName: "",
-    // middleName: "",
-    // lastName: "",
-    // address: "",
-    // phone: "",
-    // email: "",
-    // notes: "",
-
+    firstName: JSON.parse(sessionStorage.getItem('firstName')),
+    middleName: JSON.parse(sessionStorage.getItem('middleName')),
+    lastName: JSON.parse(sessionStorage.getItem('lastName')),
+    address: JSON.parse(sessionStorage.getItem('address')),
+    phone: JSON.parse(sessionStorage.getItem('phone')),
+    email: JSON.parse(sessionStorage.getItem('email')),
+    notes: JSON.parse(sessionStorage.getItem('notes')),
 
     
+    
   };
-  componentDidMount(){
+  componentDidMount(){ 
+    let userToken = JSON.parse(sessionStorage.getItem('token'))
+    fetch(`${this.state.}`)
 
   }
   render() {
@@ -37,6 +39,8 @@ class CustomerView extends Component {
       </Card.Text>
       </Card.Body>
       </Card>
+      <Button>Edit Customer</Button>
+      <Button>Delete Customer</Button>
       <Card>
         <Card.Text>
           <h2>Order History</h2>
