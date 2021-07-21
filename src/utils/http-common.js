320a46a1dd7b12ced0024ec05bcdddf5ab7835b6
ivwu22 <ivywu6@gmail.com>
2021-07-21 11:17:44 -0400

    updates baseURL

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const { NODE_ENV } = process.env;
 
-// const baseURL =
-//   NODE_ENV === "development"
-//     ? "http://localhost:8080/api"
-//     : "https://amexcsrapi.azurewebsites.net/";
+const baseURL =
+  NODE_ENV === "development"
+    ? "http://localhost:3004/api"
+    : "https://amexcsrapi.azurewebsites.net/";
 
-const baseURL = "http://localhost:3004/api";
+// const baseURL = "http://localhost:3004/api";
 
 const http = axios.create({
   baseURL,
