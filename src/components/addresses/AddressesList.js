4ff6a152898083cc2f6124a4afce5c629f9d9976
jlawrence2171 <81828527+jlawrence2171@users.noreply.github.com>
2021-07-22 17:23:48 -0400

    saving changes in css file

--- src/components/addresses/AddressesList.js
+++ src/components/addresses/AddressesList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import AddressDataService from "../../services/address.data.service";
-import "./AddressesList.css";
+import AddressesListCSS from "./AddressesList.module.css";
 import { Container, Row, Col } from "react-bootstrap";
 
 class AddressesList extends Component {
@@ -16,39 +16,43 @@ class AddressesList extends Component {
       .catch(console.error);
   }
   render() {
-        
+
     const { addresses } = this.state;
     const addressListItems = addresses.map((address, index) => (
-      <li key={`${address.zip}-${index}`}>
-        
-                     <Card className="mainCard" style={{ width: '18rem' }}>
-         <Card.Title>
-        <p>Address ID: {address.id}</p></Card.Title>
-        <p>Address Line 1: {address.address_line_1}</p>
-        <p>Address Line 2: {address.address_line_2}</p>
-        <p>City: {address.city}</p>
-        <p>State: {address.state}</p>
-        <p>Zip: {address.zip}</p>
-        <>
-        <Button className="inline">Edit</Button>{' '} 
-        <Button className="inline delBtn">Delete</Button>{' '}
-        </>
+      <ul key={`${address.zip}-${index}`}>
+        <Card className={AddressesListCSS.addressesCards} style={{ width: '20rem' }}>
+          <Card.Title>
+            <p>Address ID: {address.id}</p>
+          </Card.Title>
+          <p>Address Line 1: {address.address_line_1}</p>
+          <p>Address Line 2: {address.address_line_2}</p>
+          <p>City: {address.city}</p>
+          <p>State: {address.state}</p>
+          <p>Zip: {address.zip}</p>
+          <>
+            <div className={AddressesListCSS.Btn}>
+              <Button >Edit</Button>{' '}
+              <Button >Delete</Button>{' '}
+            </div>
+          </>
         </Card>
-        
-        
-      </li>
-      ));
+
+
+      </ul>
+    ));
 
     return (
       <section>
-        <h1 className="header">All Addresses</h1>
-        
-          <ol>
-            <div className="mainContainer">
-              {addressListItems}
-            </div>
-          </ol>
-             
+        <h1 className={AddressesListCSS.header}>All Addresses</h1>
+        <container fluid="md">
+          <row>
+            <ul>
+              <div className={AddressesListCSS.mainContainer}>
+                {addressListItems}
+              </div>
+            </ul>
+          </row>
+        </container>
       </section>
     );
   }
