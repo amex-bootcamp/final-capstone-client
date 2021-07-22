f966d0bdae5011a036e07c4ba72634bac50dfa60
dl86644n <dana.lopez@aol.com>
2021-07-22 14:16:08 -0400

    started styling

--- src/components/customers/CustomerView.js
+++ src/components/customers/CustomerView.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import { Card, Button, } from "react-bootstrap";
+import { Card, Button, Container, Row, Col, CardGroup } from "react-bootstrap";
 import CustomerDataService from "../../services/customer.data.service";
-
+import CustomerViewCSS from "./CustomerView.Module.css";
 
 class CustomerView extends Component {
   state = {
     customer: {},
- 
   };
-  componentDidMount() { 
+  componentDidMount() {
     const {
       match: {
         params: { id },
@@ -16,44 +15,52 @@ class CustomerView extends Component {
     } = this.props;
 
     CustomerDataService.view(id)
-    .then(({ data: customer }) => this.setState({ customer }))
-    .catch(console.error);
-
+      .then(({ data: customer }) => this.setState({ customer }))
+      .catch(console.error);
   }
   render() {
-   const { customer } = this.state;
+    const { customer } = this.state;
 
     return (
       
-      <section>
-         <h2>Customer Details</h2>
-    <div>
-      <Card>
-        <Card.Body>
-          <Card.Text>
-      <p>First Name: {customer.first_name}</p>
-      <p>Middle Name:{customer.middle_name}</p>
-      <p>Last Name:{customer.last_name}</p>
-      <p>Address: {customer.address_id}</p>
-      <p>Phone: {customer.phone}</p>
-      <p>Email: {customer.email}</p>
-      <p>Notes: {customer.notes}</p>
+        
+        <div>
+          <h2>Customer Details</h2>
+          <Container>
+            {/* <Row justify-content-center>
+                <Col> */}
+                <CardGroup>
+                  <Card className="mb-3">
+                    <Card.Text>
+                      <p>First Name: {customer.first_name}</p>
+                      <p>Middle Name:{customer.middle_name}</p>
+                      <p>Last Name:{customer.last_name}</p>
+                      <p>Address: {customer.address_id}</p>
+                      <p>Phone: {customer.phone}</p>
+                      <p>Email: {customer.email}</p>
+                      <p>Notes: {customer.notes}</p>
+                      <button className={CustomerViewCSS.btn}>
+                        Edit Customer
+                      </button>
+                      <button className={CustomerViewCSS.deletebtn}>
+                        Delete Customer
+                      </button>
+                    </Card.Text>
+                  </Card>
+                {/* </Col>
+                <Col> */}
+                  <Card>
+                    <Card.Text>
+                      <h2>Order History</h2>
+                    </Card.Text>
+                  </Card>
+                {/* </Col>
+            </Row> */}
+            </CardGroup>
+          </Container>
+        </div>
 
-      </Card.Text>
-      </Card.Body>
-      </Card>
-      <Button>Edit Customer</Button>
-      <Button>Delete Customer</Button>
-      <Card>
-        <Card.Text>
-          <h2>Order History</h2>
-        </Card.Text>
-      </Card>
-    </div> 
-      </section>
-  
     );
-    
   }
 }
 
