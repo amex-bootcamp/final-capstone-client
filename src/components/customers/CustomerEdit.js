737b05bab4ee600bea76f0ee4f1fc3bb22f129eb
Johanny Clase <jocodes28@gmail.com>
2021-07-22 10:18:23 -0400

    worked on customer-edit

--- src/components/customers/CustomerEdit.js
+++ src/components/customers/CustomerEdit.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import CustomerDataService from "../../services/customer.data.service";
 
 class CustomerEdit extends Component {
+  state = {
+    customers: [,
+  }
+
+  componentDidMount() {
+    CustomerDataService.list ()
+      .then(({data: customers}) => this.setState({customers}))
+      .catch(console.error);
+  }
+
   render() {
-    return <div>CustomerEdit</div>;
+    return (
+      const {customers} = this.state;
+      const customerListItems = customers.map((customer, index) => (
+        <li key={`${customer.id}-${index}`}>
+          <p>First Name: {customers.first_name}</p>
+          <p>Middle Name: {customers.middle_name}</p>
+          <p>Last Name: {customers.last_name}</p>
+          <p>Phone: {customers.phone}</p>
+          <p>Email: {customers.email}</p>
+          <p>Notes: {customers.notes}</p>
+        </li>
+      ))
+    );
   }
 }
 
