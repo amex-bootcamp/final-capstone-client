ec0832c24baad0a911c8e2072fad071ed11c803f
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-07-22 11:45:29 -0400

    adding basic form

--- src/components/addresses/AddressCreate.js
+++ src/components/addresses/AddressCreate.js
@@ -1,44 +1,81 @@
 import React, { Component } from "react";
 import "./AddressCreate.css";
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 
 class AddressCreate extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div>
-        Create New Address
-        <form>
-          <p>
-            Address line 1:{" "}
-            <label>
-              <input type="address_line_1" />
-            </label>{" "}
-          </p>
-          <p>
-            Address line 2:
-            <label>
-              <input type="address_line_2" />
-            </label>
-          </p>
-          <p>
-            City:
-            <label>
-              <input type="city" />
-            </label>
-          </p>
-          <p>
-            State:
-            <label>
-              <input type="state" />
-            </label>
-          </p>
-          <p>
-            Zip:
-            <label>
-              <input type="zip" />
-            </label>
-          </p>
-        </form>
-        <button>Submit</button>
+        <Container>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+        {/*TATYANA FORM: USE FOR LATER*/}
+        {/*Create New Address*/}
+        {/*<form>*/}
+        {/*  <p>*/}
+        {/*    Address line 1:{" "}*/}
+        {/*    <label>*/}
+        {/*      <input type="address_line_1" />*/}
+        {/*    </label>{" "}*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    Address line 2:*/}
+        {/*    <label>*/}
+        {/*      <input type="address_line_2" />*/}
+        {/*    </label>*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    City:*/}
+        {/*    <label>*/}
+        {/*      <input type="city" />*/}
+        {/*    </label>*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    State:*/}
+        {/*    <label>*/}
+        {/*      <input type="state" />*/}
+        {/*    </label>*/}
+        {/*  </p>*/}
+        {/*  <p>*/}
+        {/*    Zip:*/}
+        {/*    <label>*/}
+        {/*      <input type="zip" />*/}
+        {/*    </label>*/}
+        {/*  </p>*/}
+        {/*</form>*/}
+        {/*<button>Submit</button>*/}
+        {/*TATYANA FORM: USE FOR LATER*/}
+
       </div>
     );
   }
