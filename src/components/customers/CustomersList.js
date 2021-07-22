83e20b33ce854d9677c950ee76f21944a9e21a93
Joyce Young <jjyoung145@gmail.com>
2021-07-22 10:04:03 -0400

    started refactoring to react hooks

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -1,62 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import CustomerDataService from "../../services/customer.data.service";
-class CustomersList extends Component {
-  state = {
-    customers: [],
-    totalCustomer: 10,
-  };
-  componentDidMount() {
+function CustomersList() {
+  const [customer, setCustomer] = useState([]);
+  const [customerLoad, setCustomerLoad] = useState(10);
+
+  useEffect(() => {
     CustomerDataService.list()
-      .then(({ data: customers }) => this.setState({ customers }))
+      .then(({ data: customers }) => setCustomer({ customers }))
       .catch(console.error);
-  }
-  handleChange = (event) => {
-    this.setState({
-      totalCustomer: event.target.value,
-    });
-  };
-  render() {
-    const { customers, totalCustomer } = this.state;
-    const customerListItems = customers.map((customer, index) => (
-      <li key={`${customer.phone}-${index}`}>
-        <p>First Name: {customer.first_name}</p>
-        <p>Middle Name: {customer.middle_name}</p>
-        <p>Last Name: {customer.last_name}</p>
-        <p>Phone: {customer.phone}</p>
-        <p>Email: {customer.email}</p>
-        <p>Notes: {customer.notes}</p>
-        <button>Delete</button>
-      </li>
-    ));
-    return (
-      <section>
-        <h2>Customers</h2>
-        <form>
-          <h3>
-            Display:{" "}
-            <select
-              type="number"
-              name="totalCustomer"
-              value={totalCustomer}
-              onChange={this.handleChange}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>{" "}
-            Customers on the Page
-          </h3>
-        </form>
-        <ol>{customerListItems}</ol>
-      </section>
-    );
-  }
+  });
+
+  // componentDidMount() {
+  // CustomerDataService.list()
+  //   .then(({ data: customers }) => this.setState({ customers }))
+  //   .catch(console.error);
+  // }
+  // handleChange = (event) => {
+  //   this.setState({
+  //     totalCustomer: event.target.value,
+  //   });
+  // };
+  // render() {
+  //   // const { customers, totalCustomer } = this.state;
+  //   const customerListItems = customers.map((customer, index) => (
+  //     <li key={`${customer.phone}-${index}`}>
+  //       <p>First Name: {customer.first_name}</p>
+  //       <p>Middle Name: {customer.middle_name}</p>
+  //       <p>Last Name: {customer.last_name}</p>
+  //       <p>Phone: {customer.phone}</p>
+  //       <p>Email: {customer.email}</p>
+  //       <p>Notes: {customer.notes}</p>
+  //       <button>Delete</button>
+  //     </li>
+  //   ));
+  return (
+    <section>
+      <h2>Customers</h2>
+      <form>
+        <h3>
+          Display:{" "}
+          <select
+            type="number"
+            name="totalCustomer"
+            value={customerLoad}
+            onChange={(event) => setCustomerLoad(event.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>{" "}
+          Customers on the Page
+        </h3>
+      </form>
+      {/* <ol>{customerListItems}</ol> */}
+    </section>
+  );
 }
+
 export default CustomersList;
