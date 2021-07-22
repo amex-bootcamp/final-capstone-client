2a9e5606464d5729d834ef2c755923f47ddc9dea
Ning Li <n.li9402@gmail.com>
2021-07-22 15:27:11 -0400

    finished customerCreate file

--- src/components/customers/CustomerCreate.js
+++ src/components/customers/CustomerCreate.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import CustomerDataService from "../../services/customer.data.service";
+import AddressDataService from "../../services/address.data.service";
 
 class CustomerCreate extends Component {
   state = {
+    submitted: false,
     first_name: "",
     middle_name: "",
     last_name: "",
     phone: "",
     email: "",
     notes: "",
+    address_id: null,
+    addresses: [],
   };
 
   handleChange = (event) => {
-    console.log("Event testing", event);
-
     this.setState({
       [event.target.name]: event.target.value,
     });
@@ -29,6 +31,7 @@ class CustomerCreate extends Component {
       phone: this.state.phone,
       email: this.state.email,
       notes: this.state.notes,
+      address_id: this.state.address_id,
     };
     CustomerDataService.post(params)
       .then((res) => {
@@ -43,13 +46,17 @@ class CustomerCreate extends Component {
   };
 
   // For address data, load address data
-  // componentDidMount() {
-  //
-  //     .then(({ data: addresses }) => this.setState({ addresses }))
-  //     .catch(console.error);
-  // }
+  componentDidMount() {
+    AddressDataService.list()
+      .then(({ data: addresses }) => this.setState({ addresses }))
+      .catch(console.error);
+  }
 
   render() {
+    let addressOptions = this.state.addresses.map((address, id) => (
+      <option value={address.id}>{address.address_line_1}</option>
+    ));
+
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -113,6 +120,17 @@ class CustomerCreate extends Component {
                 value={this.state.notes}
                 onChange={this.handleChange}
               ></input>
+            </div>
+            <div>
+              <label htmlFor="addresses">Addresses: </label>
+              <select
+                value={this.state.address_id}
+                name="address_id"
+                onChange={this.handleChange}
+              >
+                <option value="address_id">Select an address</option>
+                {addressOptions}
+              </select>
             </div>
             <button className="abutton">SUBMIT</button>
           </div>
