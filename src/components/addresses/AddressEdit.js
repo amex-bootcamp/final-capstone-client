3e14bb90819d85c9d9870eb7a8baa937273f8cfd
Daniel de la Huerta <ddelahuerta@gmail.com>
2021-07-23 15:29:54 -0400

    form in card and container

--- src/components/addresses/AddressEdit.js
+++ src/components/addresses/AddressEdit.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Container } from "react-bootstrap";
 import AddressDataService from "../../services/address.data.service";
 import Form from "react-bootstrap/Form";
 
@@ -67,75 +67,77 @@ class AddressEdit extends Component {
   render() {
     return (
       <section>
-        <h2>Address Details</h2>
         <div>
           {/* Bootstrap test */}
-          <Card>
-            <Card.Body>
-              <Form>
-                {/* Address 1 */}
-                <Form.Group className="mb-3" controlId="formAddress_1">
-                  <Form.Label>Address 1</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={this.state.address_line_1}
-                    onChange={this.handleAddressLine1}
-                    placeholder="Street Address or P.O. Box"
-                  />
-                  <Form.Text className="text-muted">
-                    Include full address
-                  </Form.Text>
-                </Form.Group>
-                {/* Address 2 */}
-                <Form.Group className="mb-3" controlId="formAddress_2">
-                  <Form.Label>Address 2</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={this.state.address_line_2}
-                    onChange={this.handleAddressLine2}
-                    placeholder="Apt, Suite, Unit, Building Floor, Etc."
-                  />
-                  {/* City */}
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formAddress_2">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={this.state.city}
-                    onChange={this.handleCity}
-                    placeholder="Deluth"
-                  />
-                </Form.Group>
-                {/* State */}
-                <Form.Group className="mb-3" controlId="formAddress_2">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={this.state.state}
-                    onChange={this.handleState}
-                    placeholder="Minnesota"
-                  />
-                </Form.Group>
-                {/* Zip */}
-                <Form.Group className="mb-3" controlId="formAddress_2">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={this.state.zip}
-                    onChange={this.handleZip}
-                    placeholder="111111"
-                  />
-                </Form.Group>
-                {/* Confirm Check Box */}
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Confirm Edit" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
+          <Container>
+            <Card>
+              <Card.Body>
+                <Card.Title>Address Details</Card.Title>
+                <Form>
+                  {/* Address 1 */}
+                  <Form.Group className="mb-3" controlId="formAddress_1">
+                    <Form.Label>Address 1</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.address_line_1}
+                      onChange={this.handleAddressLine1}
+                      placeholder="Street Address or P.O. Box"
+                    />
+                    <Form.Text className="text-muted">
+                      Include full address
+                    </Form.Text>
+                  </Form.Group>
+                  {/* Address 2 */}
+                  <Form.Group className="mb-3" controlId="formAddress_2">
+                    <Form.Label>Address 2</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.address_line_2}
+                      onChange={this.handleAddressLine2}
+                      placeholder="Apt, Suite, Unit, Building Floor, Etc."
+                    />
+                    {/* City */}
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formAddress_2">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.city}
+                      onChange={this.handleCity}
+                      placeholder="Deluth"
+                    />
+                  </Form.Group>
+                  {/* State */}
+                  <Form.Group className="mb-3" controlId="formAddress_2">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.state}
+                      onChange={this.handleState}
+                      placeholder="Minnesota"
+                    />
+                  </Form.Group>
+                  {/* Zip */}
+                  <Form.Group className="mb-3" controlId="formAddress_2">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.zip}
+                      onChange={this.handleZip}
+                      placeholder="111111"
+                    />
+                  </Form.Group>
+                  {/* Confirm Check Box */}
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Confirm Edit" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Container>
           {/* accepted */}
           {/* <form>
             <Card>
