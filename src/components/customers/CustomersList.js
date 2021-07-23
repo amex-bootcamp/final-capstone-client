8f3534b3eb22a35c95aef2eefd19f4cc69ff370f
Brittany <owens.brittany@gmail.com>
2021-07-23 12:09:22 -0400

    initial stylings

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import CustomerDataService from "../../services/customer.data.service";
 import { Link } from "react-router-dom";
+import CustomerListCSS from "./CustomersList.module.css";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 class CustomersList extends Component {
   state = {
@@ -15,17 +17,33 @@ class CustomersList extends Component {
   render() {
     const { customers } = this.state;
     const customerListItems = customers.map((customer, index) => (
-      <li key={`${customer.phone}-${index}`}>
-        <p>Customer ID: {customer.id}</p>
-        <p>First Name: {customer.first_name}</p>
-        <p>Middle Name: {customer.middle_name}</p>
-        <p>Last Name: {customer.last_name}</p>
-        <p>Phone: {customer.phone}</p>
-        <p>Email: {customer.email}</p>
-        <p>Notes: {customer.notes}</p>
-        <p>Address ID: {customer.address_id}</p>
-        <Link to={`customers/${customer.id}`}>View Details</Link>
-      </li>
+      <ul key={`${customer.phone}-${index}`}>
+        <Card className={CustomerListCSS.wholeCard} style={{ width: "18rem" }}>
+          {/* <Card.Img variant="bottom" src="holder.js/100px180?text=Image cap" /> */}
+          <Card.Body>
+            <Card.Title>
+              {customer.first_name} {customer.middle_name.toUpperCase()}.{" "}
+              {customer.last_name}
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              # {customer.id}
+            </Card.Subtitle>
+            <Card.Text>
+              Phone: <br /> {customer.phone}
+            </Card.Text>
+            <Card.Text>{customer.email}</Card.Text>
+            <Card.Text>
+              <p>
+                Notes: <br /> {customer.notes}
+              </p>
+            </Card.Text>
+            <Card.Text>
+              <p>Address ID: {customer.address_id}</p>
+            </Card.Text>
+          </Card.Body>
+          <Link to={`customers/${customer.id}`}>View Details</Link>
+        </Card>
+      </ul>
     ));
     return (
       <section>
