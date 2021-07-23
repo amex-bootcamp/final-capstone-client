3e2eb2d93f729af168489e397a86e0ca6093bd0f
jlawrence2171 <81828527+jlawrence2171@users.noreply.github.com>
2021-07-23 10:24:31 -0400

    cards inline style used

--- src/components/addresses/AddressesList.js
+++ src/components/addresses/AddressesList.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
 import AddressDataService from "../../services/address.data.service";
+import {Card, Button, Container, Row, Col } from "react-bootstrap";
 import AddressesListCSS from "./AddressesList.module.css";
-import { Container, Row, Col } from "react-bootstrap";
 
 class AddressesList extends Component {
   state = {
@@ -16,11 +16,17 @@ class AddressesList extends Component {
       .catch(console.error);
   }
   render() {
-
+    const cardStyles = {color: "#f1faee", 
+      border: "4px solid #457b9d",
+      backgroundColor: "#1d3557" ,
+      textAlign: "center" ,
+      fontWeight: "bold" ,
+      width: '20rem' ,
+  }
     const { addresses } = this.state;
     const addressListItems = addresses.map((address, index) => (
       <ul key={`${address.zip}-${index}`}>
-        <Card className={AddressesListCSS.addressesCards} style={{ width: '20rem' }}>
+        <Card style= {cardStyles}>
           <Card.Title>
             <p>Address ID: {address.id}</p>
           </Card.Title>
