652ced12c9213d58bdf814c61ba2fbc7b0caf014
Tatyana Alexander <tatyana.theresa@gmail.com>
2021-07-23 09:41:15 -0400

    Post request on form

--- src/components/addresses/AddressCreate.js
+++ src/components/addresses/AddressCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./AddressCreate.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 class AddressCreate extends Component {
   constructor(props) {
@@ -46,15 +47,24 @@ class AddressCreate extends Component {
 
   createAddress = (event) => {
     event.preventDefault();
-    this.setState((currentState) => {
-      return {
-        address: "",
-        address2: "",
-        city: "",
-        state: "",
-        zip: "",
-      };
-    });
+    axios
+      .post("http://localhost:8080/api/addresses", {
+        address: this.state.address,
+        address2: this.state.address2,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip,
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          address: this.state.address,
+          address2: this.state.address2,
+          city: this.state.city,
+          state: this.state.state,
+          zip: this.state.zip,
+        });
+      });
   };
 
   render() {
@@ -70,36 +80,71 @@ class AddressCreate extends Component {
               <div className={"text-center"}>
                 <h2>Create New Address</h2>
               </div>
-              <Form>
+              <Form onSubmit={this.createAddress}>
                 <Form.Group className="mb-3" controlId="formGridAddress1">
                   <Form.Label>Address</Form.Label>
-                  <Form.Control placeholder="1234 Main St" />
+                  <Form.Control
+                    placeholder="1234 Main St"
+                    type="text"
+                    onChange={this.handleAddressChange}
+                    name={this.state.address}
+                    value={this.state.address}
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formGridAddress2">
                   <Form.Label>Address 2</Form.Label>
-                  <Form.Control placeholder="Apartment, studio, or floor" />
+                  <Form.Control
+                    placeholder="Apartment, studio, or floor"
+                    type="text"
+                    onChange={this.handleAddress2Change}
+                    name={this.state.address2}
+                    value={this.state.address2}
+                  />
                 </Form.Group>
 
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>City</Form.Label>
-                    <Form.Control placeholder="New York City" />
+                    <Form.Control
+                      placeholder="New York City"
+                      type="text"
+                      onChange={this.handleCityChange}
+                      name={this.state.city}
+                      value={this.state.city}
+                    />
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="formGridState">
                     <Form.Label>State</Form.Label>
-                    <Form.Control placeholder="New York" />
+                    <Form.Control
+                      placeholder="New York"
+                      type="text"
+                      onChange={this.handleStateChange}
+                      name={this.state.state}
+                      value={this.state.state}
+                    />
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="formGridZip">
                     <Form.Label>Zip</Form.Label>
-                    <Form.Control placeholder="12345" />
+                    <Form.Control
+                      placeholder="12345"
+                      type="text"
+                      onChange={this.handleZipChange}
+                      name={this.state.zip}
+                      value={this.state.zip}
+                    />
                   </Form.Group>
                 </Row>
 
                 <div className={"text-center"}>
-                  <Button variant="primary" size="lg" type="submit">
+                  <Button
+                    variant="primary"
+                    size="lg"
+                    type="submit"
+                    value="submit"
+                  >
                     Submit
                   </Button>
                 </div>
