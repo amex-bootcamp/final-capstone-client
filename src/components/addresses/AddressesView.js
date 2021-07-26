89f6c41de43a96f7853bf283344200800b730d89
Alpha Bah <Bahalphapha2096@gmail.com>
2021-07-26 15:31:36 -0400

    updated the address view code into its actual branch

--- src/components/addresses/AddressesView.js
+++ src/components/addresses/AddressesView.js
@@ -1,61 +1,83 @@
-// import React, { Component } from "react";
-// import AddressDataService from "../../services/address.data.service";
-// import CustomerViewCSS from "./CustomerView.Module.css";
-// import { Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import { Card, Container, CardGroup, Button } from "react-bootstrap";
+import AddressView from "./AddressView.css";
+import AddressDataService from "../../services/address.data.service";
+import { Redirect } from "react-router-dom";
 
-// class AddressesView extends Component {
-//   state = {
-//     address: [
-//       {
-//         deleted: false,
-//       },
-//     ],
-//   };
+class AddressesView extends Component {
+  state = {
+    address: [
+      {
+        deleted: false,
+      },
+    ],
+  };
 
-//   componentDidMount() {
-//     const {
-//       match: {
-//         params: { id },
-//       },
-//     } = this.props;
+  componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
 
-//     AddressDataService.view(id)
-//       .then(({ data: address }) => 
-//       this.setState({ address }))
-//       .catch(console.error);
-//   }
+    AddressDataService.view(id)
+      .then(({ data: address }) => 
+      this.setState({ address }))
+      .catch(console.error);
+  }
 
-//   deleteAddress(id) {
-//     AddressDataService.delete(id)
-//       .then((res) => {
-//       console.log(res);
-//       console.log(res.data);
-//       this.setState({ deleted: true });
-//     });
-//   }
+  deleteAddress(id) {
+    AddressDataService.delete(id)
+      .then((res) => {
+      console.log(res);
+      console.log(res.data);
+      this.setState({ deleted: true });
+    });
+  }
 
-//   render() {
-//     const { address } = this.state;
-//     if (this.state.deleted) {
-//       return <Redirect to={{ pathname: "/addresses" }} />;
-//     }
+  render() {
+    const { address } = this.state;
+    if (this.state.deleted) {
+      return <Redirect to={{ pathname: "/addresses" }} />;
+    }
 
-//     return (
-//       <div>
-//         const {addresses} = this.state; const addressListItems =
-//         addresses.map((address, index) => (
-//         <li key={`${address.zip}-${index}`}>
-//           <p>Address ID: {address.id}</p>
-//           <p>Address Line 1: {address.address_line_1}</p>
-//           <p>Address Line 2: {address.address_line_2}</p>
-//           <p>City: {address.city}</p>
-//           <p>State {address.state}</p>
-//           <p>Zip: {address.zip}</p>
-//         </li>
-//         ));
-//       </div>
-//     );
-//   }
-// }
+    return (
+        <div>
+          
+          <Container className={AddressView.container}>
+            {/* <Row justify-content-center>
+                <Col> */}
+                <CardGroup>
+                  <Card className={AddressView.card}>
+                    <Card.Text>
+                    <h2 className={AddressView.h2}>Address</h2>
+                      <span className={AddressView.s}>Address Line 1:</span> {address.data[0].aadress_line_1} <br/>
+                      <span className={AddressView.s}>Address Line 2:</span> {address.data[0].aadress_line_2} <br/>
+                      <span className={AddressView.s}>City:</span> {address.data[0].city} <br/>
+                      <span className={AddressView.s}>State:</span> {address.data[0].state} <br/>
+                     <span className={AddressView.s}>Zip: </span>{address.data[0].zip} <br/>
+                    </Card.Text>
+                    <div flex className={AddressView.btndiv}>
+                      <button className={AddressView.btn}>
+                        Edit Customer
+                      </button>
+                      <button onClick={() => this.deleteAddress(address.id)} className={AddressView.deletebtn}>
+                        Delete Customer
+                      </button>
+                      </div>
+                  </Card>
+                  <Card className={AddressView.card}>
+                    <Card.Text>
+                      <h2 className={AddressView.h2}>Order History</h2>
+                    </Card.Text>
+                  </Card>
+                {/* </Col>
+            </Row> */}
+            </CardGroup>
+          </Container>
+        </div>
+    );
+  }
+}
 
-// export default AddressesView;
+export default AddressesView; 
