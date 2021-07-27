8a5a546f0166e89f28241e2363f00c64b3bc6261
Joyce Young <jjyoung145@gmail.com>
2021-07-27 13:57:51 -0400

    added productlist dropdown

--- src/components/orders/OrderCreate.js
+++ src/components/orders/OrderCreate.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import OrderDataService from "../../services/order.data.service";
 import CustomerDataService from "../../services/customer.data.service";
+import ProductDataService from "../../services/product.data.service";
 import { Form } from "react-bootstrap";
 
 function OrderCreate() {
@@ -8,6 +9,10 @@ function OrderCreate() {
   const [order, setOrder] = useState([]);
   const [customer, setCustomer] = useState([]);
   const [customerId, setCustomerId] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [productPrice, setProductPrice] = useState(0);
+  const [datetime] = useState(new Date());
 
   useEffect(() => {
     OrderDataService.list()
@@ -15,37 +20,59 @@ function OrderCreate() {
         setOrder(data.data);
       })
       .catch(console.error);
-  }, []);
-  useEffect(() => {
     CustomerDataService.list()
       .then((data) => {
         setCustomer(data.data);
       })
       .catch(console.error);
+    ProductDataService.list()
+      .then((data) => {
+        setProducts(data.data);
+      })
+      .catch(console.error);
   }, []);
+
+  const customerIdOption = customer.map((customer, id) => (
+    <option value={customer.id} key={`${customer.id}-${id}`}>
+      {"ID: " +
+        customer.id +
+        " " +
+        customer.first_name +
+        " " +
+        customer.last_name}
+    </option>
+  ));
+
+  const productOption = products.map((product, id) => (
+    <option value={product.price} key={`${product.id}-${id}`}>
+      {" " + product.name + " $" + product.price}
+    </option>
+  ));
 
   return (
     <section>
-      <h2>Create a New Ordder</h2>
+      <h2>Create a New Order</h2>
       <form>
-        <label>
-          Order ID:
-          <input
-            id="orderId"
-            name="orderId"
-            type="text"
-            value={Math.max(...order.map((order) => order.id)) + 1}
-            readOnly
-          ></input>
-        </label>
+        <label>Order ID:</label>
+        <input
+          id="orderId"
+          name="orderId"
+          type="text"
+          value={Math.max(...order.map((order) => order.id)) + 1}
+          readOnly
+        ></input>
+        <br />
+        <label>Customer ID: </label>
         <select
           type="text"
           name="customerId"
           value={customerId}
           onChange={(event) => setCustomerId(event.target.value)}
         >
-          <option></option>
+          {customerIdOption}
         </select>
+        <br />
+        <label>Order Status: </label>
         <select
           type="type"
           name="orderStatus"
@@ -61,7 +88,32 @@ function OrderCreate() {
           <option value="delivered">Delivered</option>
           <option value="closed">Closed</option>
         </select>
+        <br />
+        <label>Date/Time: </label>
+        <input
+          id="date"
+          name="date"
+          type="text"
+          value={datetime}
+          readOnly
+        ></input>
+        <br />
+        <label>Order Notes: </label>
+        <textarea id="orderNotes" name="orderNotes" type="text"></textarea>
+        <br />
+        <label for="productList">Products: </label>
+        <select
+          multiple
+          id="productList"
+          type="text"
+          name="productList"
+          // value={productId}
+          onChange={(event) => setProductPrice(event.target.value)}
+        >
+          {productOption}
+        </select>
       </form>
+      <p>{productPrice}</p>
     </section>
   );
 }
