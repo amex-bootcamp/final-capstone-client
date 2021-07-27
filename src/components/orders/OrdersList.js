3937445f1fbb3a1382b950b80cb64fcd87c1149a
Ning Li <n.li9402@gmail.com>
2021-07-27 14:55:43 -0400

    new orderLists

--- src/components/orders/OrdersList.js
+++ src/components/orders/OrdersList.js
@@ -4,15 +4,17 @@ import { Redirect, Link } from "react-router-dom";
 
 function OrdersList() {
   const [orders, setOrders] = useState([]);
+
   useEffect(() => {
     OrderDataService.list()
       .then(({ data: orders }) => setOrders(orders))
       .catch(console.error);
   }, []);
+
   const orderListItems = orders.map((order, index) => (
     <li key={`${order}-${index}`}>
       <p>ID: {order.id}</p>
-      <p>Order Status: {order.status_text}</p>
+      <p>Order Status: {order.order_status}</p>
       <p>Date Order Placed: {order.datetime_order_placed}</p>
       <p>First Name: {order.Customer.first_name}</p>
       <p>Last Name: {order.Customer.last_name}</p>
@@ -20,9 +22,12 @@ function OrdersList() {
       <Link to={`orders/${order.id}`}>View Details</Link>
     </li>
   ));
+
   return (
     <div>
       <ol>{orderListItems}</ol>
     </div>
   );
 }
+
+export default OrdersList;
