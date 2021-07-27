22d0af43729e605ff42af4b63b49e80d5e0ce91c
Joyce Young <jjyoung145@gmail.com>
2021-07-27 17:34:36 -0400

    made certain components required, finished post method, added more to db

--- src/components/orders/OrderCreate.js
+++ src/components/orders/OrderCreate.js
@@ -1,162 +1,9 @@
-// import React, { useState, useEffect } from "react";
-// import OrderDataService from "../../services/order.data.service";
-// import CustomerDataService from "../../services/customer.data.service";
-// import ProductDataService from "../../services/product.data.service";
-// import { Form } from "react-bootstrap";
-
-// function OrderCreate() {
-//   const [orderStatus, setOrderStatus] = useState("");
-//   const [order, setOrder] = useState([]);
-//   const [customer, setCustomer] = useState([]);
-//   const [customerId, setCustomerId] = useState(0);
-//   const [products, setProducts] = useState([]);
-//   const [productPrices, setProductPrices] = useState([0]);
-//   const [datetime] = useState(new Date());
-//   const [productId, setProductId] = useState([]);
-//   const [orderNotes, setOrderNotes] = useState("");
-
-//   useEffect(() => {
-//     OrderDataService.list()
-//       .then((data) => {
-//         setOrder(data.data);
-//       })
-//       .catch(console.error);
-//     CustomerDataService.list()
-//       .then((data) => {
-//         setCustomer(data.data);
-//       })
-//       .catch(console.error);
-//     ProductDataService.list()
-//       .then((data) => {
-//         setProducts(data.data);
-//       })
-//       .catch(console.error);
-//   }, []);
-
-//   const customerIdOption = customer.map((customer, id) => (
-//     <option value={customer.id} key={`${customer.id}-${id}`}>
-//       {"ID: " +
-//         customer.id +
-//         " " +
-//         customer.first_name +
-//         " " +
-//         customer.last_name}
-//     </option>
-//   ));
-
-//   const productOption = products.map((product, id) => (
-//     <option value={product.price} id={product.id} key={`${product.id}-${id}`}>
-//       {" " + product.name + " $" + product.price}
-//     </option>
-//   ));
-
-//   const totalPrice = productPrices.reduce((x, y) => Number(x) + Number(y));
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const params = {
-//       customer_id: customerId,
-//       order_status: orderStatus,
-//       datetime_order_place: datetime,
-//       total_order_price: totalPrice,
-//       order_notes: orderNotes,
-//     };
-//     OrderDataService.post(params)
-//       .then((res) => {
-//         console.log(res.data);
-//       })
-//       .catch((error) => console.log(error));
-//   };
-
-//   return (
-//     <section>
-//       <h2>Create a New Order</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>Order ID:</label>
-//         <input
-//           id="orderId"
-//           name="orderId"
-//           type="text"
-//           value={Math.max(...order.map((order) => order.id)) + 1}
-//           readOnly
-//         ></input>
-//         <br />
-//         <label>Customer ID: </label>
-//         <select
-//           type="text"
-//           name="customerId"
-//           value={customerId}
-//           onChange={(event) => setCustomerId(event.target.value)}
-//         >
-//           {customerIdOption}
-//         </select>
-//         <br />
-//         <label>Order Status: </label>
-//         <select
-//           type="type"
-//           name="orderStatus"
-//           value={orderStatus}
-//           onChange={(event) => setOrderStatus(event.target.value)}
-//         >
-//           <option value="draft">Draft</option>
-//           <option value="open">Open</option>
-//           <option value="finalized">Finalized</option>
-//           <option value="prep">Preparing to Ship</option>
-//           <option value="ready">Ready for Shipping</option>
-//           <option value="shipped">Shipped</option>
-//           <option value="delivered">Delivered</option>
-//           <option value="closed">Closed</option>
-//         </select>
-//         <br />
-//         <label>Date/Time: </label>
-//         <input
-//           id="date"
-//           name="date"
-//           type="text"
-//           value={datetime}
-//           readOnly
-//         ></input>
-//         <br />
-//         <label>Order Notes: </label>
-//         <textarea
-//           id="orderNotes"
-//           name="orderNotes"
-//           type="text"
-//           onChange={(event) => {
-//             setOrderNotes(event.target.value);
-//           }}
-//         ></textarea>
-//         <br />
-//         <label for="productList">Products: </label>
-//         <select
-//           multiple
-//           id="productList"
-//           type="text"
-//           name="productList"
-//           onChange={(event) => {
-//             setProductPrices(
-//               Array.from(event.target.selectedOptions, (option) => option.value)
-//             );
-//             setProductId(
-//               Array.from(event.target.selectedOptions, (option) => option.id)
-//             );
-//           }}
-//         >
-//           {productOption}
-//         </select>
-//         <p>Total Price: {totalPrice}</p>
-//         <button type="submit">Submit</button>
-//       </form>
-//     </section>
-//   );
-// }
-
-// export default OrderCreate;
 import React, { useState, useEffect } from "react";
 import OrderDataService from "../../services/order.data.service";
 import CustomerDataService from "../../services/customer.data.service";
 import ProductDataService from "../../services/product.data.service";
 import { Form } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
 
 function OrderCreate() {
   const [orderStatus, setOrderStatus] = useState("");
@@ -168,6 +15,8 @@ function OrderCreate() {
   const [datetime] = useState(new Date());
   const [productId, setProductId] = useState([]);
   const [orderNotes, setOrderNotes] = useState("");
+  const [sumbitted, setSubmitted] = useState(false);
+  let orderId = Math.max(...order.map((order) => order.id)) + 1;
 
   useEffect(() => {
     OrderDataService.list()
@@ -207,23 +56,30 @@ function OrderCreate() {
   const totalPrice = productPrices.reduce((x, y) => Number(x) + Number(y));
 
   const handleSubmit = (event) => {
-    event.preventDefault();
     const params = {
+      id: orderId,
       customer_id: customerId,
       order_status: orderStatus,
       datetime_order_place: datetime,
       total_order_price: totalPrice,
       order_notes: orderNotes,
+      products: productId,
     };
     OrderDataService.post(params)
       .then((res) => {
         console.log(res.data);
       })
       .catch((error) => console.log(error));
+
+    setSubmitted(true);
   };
 
+  if (sumbitted === true) {
+    return <Redirect to="/orders" />;
+  }
+
   return (
-    <>
+    <section>
       <h2>Create a New Order</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="orderId">
@@ -232,7 +88,7 @@ function OrderCreate() {
             id="orderId"
             name="orderId"
             type="text"
-            value={Math.max(...order.map((order) => order.id)) + 1}
+            value={orderId}
             readOnly
           />
         </Form.Group>
@@ -243,7 +99,9 @@ function OrderCreate() {
             name="customerId"
             value={customerId}
             onChange={(event) => setCustomerId(event.target.value)}
+            required
           >
+            <option value="">--- SELECT A CUSTOMER ---</option>
             {customerIdOption}
           </Form.Select>
         </Form.Group>
@@ -254,7 +112,9 @@ function OrderCreate() {
             name="orderStatus"
             value={orderStatus}
             onChange={(event) => setOrderStatus(event.target.value)}
+            required
           >
+            <option value="">--- SELECT AN ORDER STATUS ---</option>
             <option value="draft">Draft</option>
             <option value="open">Open</option>
             <option value="finalized">Finalized</option>
@@ -292,6 +152,7 @@ function OrderCreate() {
           <Form.Select
             as="select"
             multiple
+            required
             id="productList"
             type="text"
             name="productList"
@@ -311,11 +172,11 @@ function OrderCreate() {
           </Form.Select>
         </Form.Group>
         <Form.Group controlId="totalPrice">
-          <Form.Label>Total Price: {totalPrice}</Form.Label>
+          <Form.Label>Total Price: ${totalPrice}</Form.Label>
         </Form.Group>
         <button type="submit">Submit</button>
       </Form>
-    </>
+    </section>
   );
 }
 
