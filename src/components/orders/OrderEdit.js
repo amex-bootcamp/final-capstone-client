4caca769674c91b7f5fa5385575e6472014372ee
Daniel de la Huerta <ddelahuerta@gmail.com>
2021-07-28 15:09:05 -0400

    increment button plus bootstrap styling

--- src/components/orders/OrderEdit.js
+++ src/components/orders/OrderEdit.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import CustomerEditCSS from "../customers/CustomerEdit.module.css";
-import { Card, Container, CardGroup, Button, Dropdown } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  CardGroup,
+  Button,
+  Dropdown,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 // import OrderDataService from "../../services/order.data.service";
 
 class OrderEdit extends Component {
   state = {
+    count: 0,
     edited: false,
     id: "",
     first_name: "",
@@ -41,6 +51,15 @@ class OrderEdit extends Component {
   //     .catch(console.error);
   // }
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     if (this.state.edited) {
       return <Redirect to={{ pathname: `/customers/${this.state.id}` }} />;
@@ -48,13 +67,97 @@ class OrderEdit extends Component {
     return (
       <section>
         <div>
+          <span className="button button-primary m-2">{this.formatCount()}</span>
+          <Button onClick={this.handleIncrement}>Increment</Button>
           <Container className={CustomerEditCSS.container}>
             <CardGroup>
               <Card>
                 <Card.Body>
                   <Card.Text>
-                    <h2 className={CustomerEditCSS.h2}>Edit Orders</h2>
-                    <form
+                    {/* <h2 className={CustomerEditCSS.h2}>Edit Orders</h2> */}
+                    <Form onSubmit={this.handleSubmit}>
+                      <Row>
+                        <Col>
+                          <Form.Label>Name</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={this.state.first_name}
+                            name="first_name"
+                            id="first_name"
+                            onChange={this.handleFirstNameChange}
+                            placeholder="Name"
+                          />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Form.Label>Middle Name</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={this.state.middle_name}
+                            id="middle_name"
+                            onChange={this.handleMiddleNameChange}
+                            placeholder="Middle Name"
+                          />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Form.Label>Last Name</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={this.handleLastNameChange}
+                            id="last_name"
+                            onChange={this.handleLastNameChange}
+                            placeholder="Last Name"
+                          />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Form.Label>Phone</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={this.state.phone}
+                            id="phone"
+                            onChange={this.handlePhoneChange}
+                            placeholder="Phone"
+                          />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={this.state.email}
+                            id="email"
+                            onChange={this.handleEmailChange}
+                            placeholder="Email"
+                          />
+                        </Col>
+                      </Row>
+                      <Form.Group as={Col} controlId="orderStatus">
+                        <Form.Label>Order Status</Form.Label>
+                        <Form.Select defaultValue="Choose...">
+                          <option>Open</option>
+                          <option>Finalized</option>
+                          <option>Ready To Ship</option>
+                          <option>Shipped</option>
+                          <option>Complete</option>
+                        </Form.Select>
+                      </Form.Group>
+                      <br></br>
+                      <Button
+                        // className={CustomerEditCSS.savebtn}
+                        variant="primary"
+                        type="submit"
+                        value="submit"
+                      >
+                        Update Order
+                      </Button>
+                    </Form>
+                    {/* <form
                       className={CustomerEditCSS.formcenter}
                       onSubmit={this.handleSubmit}
                     >
@@ -120,9 +223,9 @@ class OrderEdit extends Component {
                             Order Completed
                           </Dropdown.Item>
                         </Dropdown.Menu>
-                      </Dropdown>
+                      </Dropdown> */}
 
-                      {/* <label>Order Status:</label> <br></br>
+                    {/* <label>Order Status:</label> <br></br>
                         <select
                           name="title"
                           
@@ -133,7 +236,7 @@ class OrderEdit extends Component {
                           <option value="Miss">Miss</option>
                           <option value="Ms">Ms</option>
                         </select> */}
-                      <p>
+                    {/* <p>
                         <Dropdown>
                           <Dropdown.Toggle
                             variant="success"
@@ -176,7 +279,7 @@ class OrderEdit extends Component {
                           Update Order
                         </Button>
                       </p>
-                    </form>
+                    </form> */}
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -190,6 +293,11 @@ class OrderEdit extends Component {
         </div>
       </section>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
