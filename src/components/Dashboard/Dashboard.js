09911e68cdb95dcf59d93dd297c958305b029377
Tatyana Alexander <tatyana.theresa@gmail.com>
2021-07-27 22:26:22 -0400

    Added background image to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,14 +3,18 @@ import React, { Component } from "react";
 import { Image } from "react-bootstrap";
 class Dashboard extends Component {
   render() {
+    const backgroundImage = {
+      backgroundPosition: "center",
+      backgroundRepeat: "repeat",
+      backgroundSize: "cover",
+      backgroundImage: "url(/images/droneSetOutPic.jpg)",
+      width: "100vw",
+      height: "100vh",
+    };
     return (
-      <>
-        <h1>Dashboard</h1>
-        <Image
-          src={process.env.PUBLIC_URL + "/images/longDronePic.jpg"}
-          alt="longDronePic"
-        />
-      </>
+      <div style={backgroundImage}>
+        <h1>CSR Dashboard</h1>
+      </div>
     );
   }
 }
