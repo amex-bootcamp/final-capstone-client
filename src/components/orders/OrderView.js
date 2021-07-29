8359f4393cb610295e36e1f529abfe5ce3a0d92c
Andre Brown <andreups@msn.com>
2021-07-29 11:57:14 -0400

    working modal

--- src/components/orders/OrderView.js
+++ src/components/orders/OrderView.js
@@ -1,57 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Card, Container, Button, Modal } from "react-bootstrap";
+import { useParams, Redirect } from "react-router-dom";
 import OrderViewCSS from "../orders/OrderView.module.css";
 import OrderDataService from "../../services/order.data.service";
 
 function OrderView() {
-  const [order, setOrder] = useState( { Order: { data: {}, deleted: false }, show: false, } );
+  const [order, setOrder] = useState({ Customer: {}, deleted: false});
+  const [show, setShow] = useState(false);
   const { id } = useParams();
-
-  useEffect( () => {
+  useEffect(() => {
     getOrderData();
-  }, [] );
+  }, []);
   function getOrderData() {
-    OrderDataService.view( id )
-      .then( ( { data: order } ) => setOrder( order ) )
-      .catch( console.error );
+    OrderDataService.view(id)
+      .then(({ data: order }) => setOrder(order))
+      .catch(console.error);
   }
-
-  const setShow = () => {
-    setOrder( [...order, { show: !order.show }] )
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow((currentState) => {
+      return {
+        show: !currentState.show,
+      };
+    });
   };
-
-  const deleteCustomer = ( id ) => {
-    OrderDataService.delete( id )
-    .then( ( data ) => {
-      setOrder( { deleted: true } );
-    })
-  };
-
-  const handleClose = () => setShow();
-  const handleShow = () => setShow();
 
   const handleConfirm = () => {
-    handleShow();
-
+    OrderDataService.delete(id)
+      .then((res) => {
+        console.log("Complete");
+        setOrder( { deleted: true } );
+      })
+      .catch(console.error);
+    handleClose();
+      return <Redirect to={{ pathname: "/orders" }} />;
   };
-
-
-  // handleShow = (id) => {
-  //   setShow();
-  //   setOrder({...order, selectedOrder: id });
-  // };
-  // handleConfirm = () => {
-  //   const { selectedOrder } = useState([]);
-  //   handleClose();
-  //   OrderDataService.delete(selectedOrder)
-  //     .then(() => {
-  //       setOrder({ deleted: true });
-  //     })
-  //     .catch(console.error);
-  // };
-  //   });
-  // }
 
   return (
     <div>
@@ -64,7 +47,7 @@ function OrderView() {
           <p className={OrderViewCSS.para}>Order ID: {order.id}</p>
           <p className={OrderViewCSS.para}>Customer ID: {order.customer_id}</p>
           <p className={OrderViewCSS.para}>
-            Customer First Name: {order.Customer.first_name}i
+            Customer First Name: {order.Customer.first_name}
           </p>
           <p className={OrderViewCSS.para}>
             Customer Last Name: {order.Customer.last_name}
@@ -85,30 +68,30 @@ function OrderView() {
           <p className={OrderViewCSS.para}>Order Notes: {order.order_notes}</p>
           <div flex className={OrderViewCSS.btndiv}>
             <Button className={OrderViewCSS.editbtn}>Edit</Button>
-            <Button className={OrderViewCSS.deletebtn}>Delete</Button>
+            <Button onClick={handleShow} className={OrderViewCSS.deletebtn}>
+              Delete
+            </Button>
           </div>
         </Card>
       </Container>
-      {/* <Modal show={handleShow} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              Are you sure you want to delete this order?
-            </Modal.Title>
-          </Modal.Header>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure you want to delete this order?</Modal.Title>
+        </Modal.Header>
 
-          <Modal.Footer>
-            <Button type="radio" variant="danger" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button
-              type="radio"
-              variant="primary"
-              onClick={() => handleConfirm(order.id)}
-            >
-              Confirm
-            </Button>
-          </Modal.Footer>
-        </Modal> */}
+        <Modal.Footer>
+          <Button type="radio" variant="danger" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            type="radio"
+            variant="primary"
+            onClick={() => handleConfirm(id) }
+          >
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
