01bff0cdac0cb3ee7c9ff503179cedc940c4919c
ivwu22 <ivywu6@gmail.com>
2021-07-29 12:01:03 -0400

    fixes addressedit to work

--- src/components/addresses/AddressEdit.js
+++ src/components/addresses/AddressEdit.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 import AddressDataService from "../../services/address.data.service";
+import { Redirect } from "react-router-dom";
 
 class AddressEdit extends Component {
   state = {
@@ -9,27 +10,29 @@ class AddressEdit extends Component {
     city: "",
     state: "",
     zip: "",
+    edited: false,
+    id: "",
   };
 
-  componentDidMount() {
-    const {
-      match: {
-        params: { id },
-      },
-    } = this.props;
+  // componentDidMount() {
+  //   const {
+  //     match: {
+  //       params: { id },
+  //     },
+  //   } = this.props;
 
-    AddressDataService.view(id)
-      .then(({ data: address }) =>
-        this.setState({
-          address_line_1: address.address_line_1,
-          address_line_2: address.address_line_2,
-          city: address.city,
-          state: address.state,
-          zip: address.zip,
-        })
-      )
-      .catch(console.error);
-  }
+  //   AddressDataService.view(id)
+  //     .then(({ data: address }) =>
+  //       this.setState({
+  //         address_line_1: address.address_line_1,
+  //         address_line_2: address.address_line_2,
+  //         city: address.city,
+  //         state: address.state,
+  //         zip: address.zip,
+  //       })
+  //     )
+  //     .catch(console.error);
+  // }
 
   handleAddressLine1 = (event) => {
     this.setState({
@@ -59,12 +62,42 @@ class AddressEdit extends Component {
     });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+
+    const params = {
+      id: id,
+      address_line_1: this.state.address_line_1,
+      address_line_2: this.state.address_line_2,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip,
+    };
+    AddressDataService.put(id, params)
+      .then((res) => {
+        this.setState({
+          edited: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
+    if (this.state.edited) {
+      return <Redirect to={{ pathname: `/addresses/${this.state.id}` }} />;
+    }
     return (
       <section>
         <h2>Address Details</h2>
         <div>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <Card>
               <Card.Body>
                 <Card.Text>
@@ -105,7 +138,9 @@ class AddressEdit extends Component {
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Button>Submit</Button>
+            <Button type="submit" value="submit">
+              Submit
+            </Button>
             <Button>Clear</Button>
           </form>
         </div>
