63897a0a38d4c19178e00778fb9fedf3e47ff630
dl86644n <dana.lopez@aol.com>
2021-07-29 12:41:08 -0400

    centered searchbar, changed color of customers heading at top of page

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -75,6 +75,16 @@ function CustomersList() {
     display: "inline-block",
   };
 
+  const h2 = {
+    color: "black",
+    paddingTop: "20px"
+  };
+
+  const searchBar ={
+    paddingTop: "20px",
+    paddingLeft: "400px"
+  }
+
   // something in here needs to be changed in order to save the data dynamically
   const customerListItems = customers.map((customer, index) => (
     <ul key={`${customer.phone}-${index}`}>
@@ -143,7 +153,7 @@ function CustomersList() {
 
   return (
     <section>
-      <div className="col-md-8">
+      <div style={searchBar} className="col-md-8">
         <div className="input-group mb-3">
           <input
             type="text"
@@ -163,7 +173,7 @@ function CustomersList() {
           </div>
         </div>
       </div>
-      <h2>Customers</h2>
+      <h2 style={h2}>Customers</h2>
       
       <form onClick={handleClick}>
         <h3>
